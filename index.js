b3e0dee9d5f72c4520e96550b511cb783396070a
Harshit <harshit.rnnh@gmail.com>
2018-05-30 01:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech = req.body.result.parameters.echoText
+  var speech = req.body.queryResult.parameters.echoText
   let responseObj={
      "fulfillmentText":speech
     ,"fulfillmentMessages":[
