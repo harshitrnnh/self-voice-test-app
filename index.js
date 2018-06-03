7c713020cf77e406786e36e5179c75b10f9c254f
Harshit <harshit.rnnh@gmail.com>
2018-06-03 19:23:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ restService.post("/audio", function(req, res) {
 //   } 
   
  let responseObj={
-   "platform": "google",
-  "type": "custom_payload",
     "payload":{
      "google":{
        "expectUserResponse": false,
