76eed6b674344e89f8d91727876abbedb9cceea2
Harshit <harshit.rnnh@gmail.com>
2018-06-03 15:42:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ restService.post("/audio", function(req, res) {
         "items": [
           {
             "simpleResponse": {
-              "ssml": speech
+              "ssml": speech,
               "displayText": "This is a SSML sample. Make sure your sound is enabled to hear the demo"
             }
           }
