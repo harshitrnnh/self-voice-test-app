e265dbd906ca38a91f2c1abcae4fba57e12eee14
Harshit <harshit.rnnh@gmail.com>
2018-06-03 16:14:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ restService.post("/audio", function(req, res) {
     //Speech Synthesis Markup Language 
     case "1":
       speech = 
-        '<speak><audio src=“https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg”>didn’t get your MP3 audio file</audio></speak>';
-      break;
+       "<speak>Here are <say-as interpret-as=\"characters\">SSML</say-as> samples. I can pause <break time=\"3\" />. I can play a sound <audio src=\"https://actions.google.com/sounds/v1/alarms/winding_alarm_clock.ogg\">your wave file</audio>. I can speak in cardinals. Your position is <say-as interpret-as=\"cardinal\">10</say-as> in line. Or I can speak in ordinals. You are <say-as interpret-as=\"ordinal\">10</say-as> in line. Or I can even speak in digits. Your position in line is <say-as interpret-as=\"digits\">10</say-as>. I can also substitute phrases, like the <sub alias=\"World Wide Web Consortium\">W3C</sub>. Finally, I can speak a paragraph with two sentences. <p><s>This is sentence one.</s><s>This is sentence two.</s></p></speak>",
+        break;
   } 
   
  let responseObj={
@@ -51,7 +51,7 @@ restService.post("/audio", function(req, res) {
         "items": [
           {
             "simpleResponse": {
-              "ssml":"<speak>Here are <say-as interpret-as=\"characters\">SSML</say-as> samples. I can pause <break time=\"3\" />. I can play a sound <audio src=\"https://actions.google.com/sounds/v1/alarms/winding_alarm_clock.ogg\">your wave file</audio>. I can speak in cardinals. Your position is <say-as interpret-as=\"cardinal\">10</say-as> in line. Or I can speak in ordinals. You are <say-as interpret-as=\"ordinal\">10</say-as> in line. Or I can even speak in digits. Your position in line is <say-as interpret-as=\"digits\">10</say-as>. I can also substitute phrases, like the <sub alias=\"World Wide Web Consortium\">W3C</sub>. Finally, I can speak a paragraph with two sentences. <p><s>This is sentence one.</s><s>This is sentence two.</s></p></speak>",
+              "ssml":speech,
               "displayText": "This is a SSML sample. Make sure your sound is enabled to hear the demo"
             }
           }
