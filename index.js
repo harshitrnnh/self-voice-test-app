44e59c8e33efe0b599e7daa3fc5f0eb47d81d941
Harshit <harshit.rnnh@gmail.com>
2018-06-03 17:59:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ restService.post("/audio", function(req, res) {
   var speech = "";
   var param = req.body.queryResult.parameters.statement_num;
   if (param == "1"){speech ="<speak>Here are <say-as interpret-as=\"characters\">SSML</say-as> samples. I can pause <break time=\"3\" />. I can play a sound <audio src=\"https://actions.google.com/sounds/v1/alarms/winding_alarm_clock.ogg\">your wave file</audio>. I can speak in cardinals. Your position is <say-as interpret-as=\"cardinal\">10</say-as> in line. Or I can speak in ordinals. You are <say-as interpret-as=\"ordinal\">10</say-as> in line. Or I can even speak in digits. Your position in line is <say-as interpret-as=\"digits\">10</say-as>. I can also substitute phrases, like the <sub alias=\"World Wide Web Consortium\">W3C</sub>. Finally, I can speak a paragraph with two sentences. <p><s>This is sentence one.</s><s>This is sentence two.</s></p></speak>";
-;}
+                   }
 //   switch (req.body.queryResult.parameters.statement_num) {
 //     //Speech Synthesis Markup Language 
 //     case "1":
@@ -45,13 +45,9 @@ restService.post("/audio", function(req, res) {
 //   } 
   
  let responseObj={
-   
- "platform": "google",
-  "type": "custom_payload",
   "payload": {
     "google": {
       "expectUserResponse": false,
-   "isSsml": true,
       "noInputPrompts": [],
       "richResponse": {
         "items": [
