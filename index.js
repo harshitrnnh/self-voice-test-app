e491b918bf225bbb45184d5633f0d79b7a6137be
Harshit <harshit.rnnh@gmail.com>
2018-06-04 11:29:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,26 +53,7 @@ restService.post("/audio", function(req, res) {
         "items":[
            {
               "simpleResponse":{
-                "ssml":"
-  <speak>
-  <seq>
-    <media begin="0.5s">
-      <speak>Who invented the Internet?</speak>
-    </media>
-    <media begin="2.0s">
-      <speak>The Internet was invented by cats.</speak>
-    </media>
-    <media soundLevel="-6db">
-      <audio
-        src="https://actions.google.com/sounds/v1/alarms/medium_bell_ringing_near.ogg"/>
-    </media>
-    <media repeatCount="3" soundLevel="+2.28dB"
-      fadeInDur="2s" fadeOutDur="0.2s">
-      <audio
-        src="https://actions.google.com/sounds/v1/alarms/medium_bell_ringing_near.ogg"/>
-    </media>
-  </seq>
-</speak>"
+                "ssml":"<speak><seq><media begin="0.5s"><speak>Who invented the Internet?</speak></media><media begin="2.0s"><speak>The Internet was invented by cats.</speak></media><media soundLevel="-6db"><audio src="https://actions.google.com/sounds/v1/alarms/medium_bell_ringing_near.ogg"/></media><media repeatCount="3" soundLevel="+2.28dB" fadeInDur="2s" fadeOutDur="0.2s"><audio src="https://actions.google.com/sounds/v1/alarms/medium_bell_ringing_near.ogg"/></media></seq></speak>"
               }
            },
            
