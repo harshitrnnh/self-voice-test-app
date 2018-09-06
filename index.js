de552fe3c6ed530d47b06887f29331e1c8b01eb7
Harshit <harshit.rnnh@gmail.com>
2018-09-06 13:54:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,18 +40,24 @@ restService.post("/audio", function(req, res) {
   
   var speech = "";
   var param = req.body.queryResult.parameters.userText;
+  if (param.includes("august") || param.includes("August")){speech ="<speak><audio src=\"https://www.dropbox.com/s/i53j4cp5ih8585d/ashwini-sentence-composite-1.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
   
+  else if (param.includes("seven") || param.includes("7") || param.includes("Seven")){speech ="<speak><audio src=\"https://www.dropbox.com/s/pkeg3jkmxyqgc64/ashwini-sentence-composite-2.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
   
-  if (param.includes("weather")){speech ="<speak><audio src=\"https://www.dropbox.com/s/ldt5awk7vh12k8i/2%20-%20Weather.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
+  else if (param.includes("yes") || param.includes("no") || param.includes("Yes") || param.includes("No")){speech ="<speak><audio src=\"https://www.dropbox.com/s/hvwui8l3uwzq8j9/ashwini-sentence-composite-3.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
   
-  else if (param.includes("hotels")|| param.includes("hotel") || param.includes("Hotels") || param.includes("Hotels")){speech ="<speak><audio src=\"https://www.dropbox.com/s/4axboff17tz9w2h/3%20-%20Hotel.mp3?dl=1\"><desc></desc>Sure! There are a few hotels that I can suggest - Hotel Lakeview is a 4 star property, and Hotel Vintage is a three star property that is available next week. I will send you an email with a complete list.</audio></speak>";}
+  //else if (param.includes("yes") || param.includes("no") || param.includes("Yes") || param.includes("No")){speech ="<speak><audio src=\"https://www.dropbox.com/s/c415nl501yf4etn/anji-sentence-composite-4.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
   
-  else if (param.includes("essentials")|| param.includes("Essentials") || param.includes("list") || param.includes("lists") || param.includes("essential") || param.includes("Essential")){speech ="<speak><audio src=\"https://www.dropbox.com/s/omuto5a94yc95p1/4%20-%20List.mp3?dl=1\"><desc></desc>Sure, i'll remember a list and remind you a few days before your travels.</audio></speak>";}
+  else if (param.includes("weather")|| param.includes("temperature") || param.includes("Weather")){speech ="<speak><audio src=\"https://www.dropbox.com/s/ihhjnq3vn7w1j8r/ashwini-sentence-composite-5.mp3?dl=1\"><desc></desc>Sounds great! The weather next week in that area will be around 18 to 23 degrees, a perfect weather for a trek.</audio></speak>";}
   
-  else if (param.includes("preparatory") || param.includes("Preparatory") || param.includes("predatory") || param.includes("Predatory") || param.includes("proprietary") || param.includes("property")  || param.includes("safety") || param.includes("list")){speech ="<speak><audio src=\"https://www.dropbox.com/s/djl5q9ob7svt3t0/5%20-%20Preparatory%20Tips.mp3?dl=1\"><desc></desc>Don’t forget to book an insurance before your trek. Make sure you carry a first aid kit at all times. Always carry some spare energy food like trail nuts and protein bars. Most importantly, stay hydrated and have a blast. This ends our first session. Please bring an A4 sheet and pen next time.</audio></speak>";}
+  else if (param.includes("hotels")|| param.includes("hotel") || param.includes("Hotels") || param.includes("Hotels")){speech ="<speak><audio src=\"https://www.dropbox.com/s/r1bj28ojnoxeylv/ashwini-sentence-composite-6.mp3?dl=1\"><desc></desc>Wonderful. Could you suggest some hotels to stay there?</audio></speak>";}
   
-  else{speech ="<speak><audio src=\"https://www.dropbox.com/s/4ljezv7jpx91r5x/Didn%27t%20Understand.mp3?dl=1\"><desc></desc>Sorry, i didn't hear that. Could you say that again?</audio></speak>";}
-//   switch (req.body.queryResult.parameters.statement_num) {
+  else if (param.includes("essentials")|| param.includes("Essentials") || param.includes("list") || param.includes("lists") || param.includes("essential") || param.includes("Essential")){speech ="<speak><audio src=\"https://www.dropbox.com/s/e2y5t5gex9kq4dl/ashwini-sentence-composite-7.mp3?dl=1\"><desc></desc>Thanks. There's a few essential items i'll have to carry. Could you add knife, flashlight, jacket to the essentials list?</audio></speak>";}
+  
+  else if (param.includes("preparatory") || param.includes("Preparatory") || param.includes("predatory") || param.includes("Predatory") || param.includes("proprietary") || param.includes("property")  || param.includes("safety") || param.includes("list")){speech ="<speak><audio src=\"https://www.dropbox.com/s/y1rszgannxtx1oo/ashwini-sentence-composite-8.mp3?dl=1\"><desc></desc>Awesome! Are there any specific preparatory safety tips i should remember Hal?</audio></speak>";}
+  
+  else{speech ="<speak><audio src=\"https://www.dropbox.com/s/g8bxyuag9hiocfk/ashwini-sentence-composite-9.mp3?dl=1\"><desc></desc>Sorry, i didn't hear that. Could you say that again?</audio></speak>";}  
+  //switch (req.body.queryResult.parameters.statement_num) {
 //     //Speech Synthesis Markup Language 
 //     case "1":
 //       speech = 
